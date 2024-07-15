c1d473f7fdbc97bebc6e812f07eaaceac77647b9
Arnav Mahalpure <100768252+AranavMahalpure@users.noreply.github.com>
2024-07-16 02:14:24 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Reverse sub array/reverse-sub-array.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	void reverseSubArray(int *arr, int n, int l, int r) {
+	    // code here
+	    int i=l-1;
+	    int j=r-1;
+	    int temp;
+	    while(i<j){
+	        temp=arr[i];
+	        arr[i]=arr[j];
+	        arr[j]=temp;
+	        i++;
+	        j--;
+	    }
+
+
+	}
+
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, l, r;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        cin >> l >> r;
+        Solution ob;
+        ob.reverseSubArray(arr, n, l, r);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
