894bff3f90458d05ed555896f907abd95fa09be3
Arnav Mahalpure <100768252+AranavMahalpure@users.noreply.github.com>
2024-07-19 15:38:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Index of the First Occurrence of pattern in a text/index-of-the-first-occurrence-of-pattern-in-a-text.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int findMatching(string text, string pat) {
+        // Code here
+        size_t pos = text.find(pat);
+   
+    if (pos != string::npos) {
+        return static_cast<int>(pos);
+    } else {
+     
+        return -1;
+    }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string text, pat;
+        cin >> text >> pat;
+        Solution obj;
+        cout << obj.findMatching(text, pat) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
