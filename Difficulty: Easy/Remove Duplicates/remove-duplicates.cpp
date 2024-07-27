b2d38136b5ce3db3b3d5e0d4887133555befb966
Arnav Mahalpure <100768252+AranavMahalpure@users.noreply.github.com>
2024-07-28 02:04:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Duplicates/remove-duplicates.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+
+    string removeDups(string str) {
+        // Your code goes here
+        string ans;
+        for (auto i : str) {
+            if (ans.find(i) != std::string::npos) {
+                continue;
+            }
+            ans += i;
+        }
+        
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+
+        Solution ob;
+        cout << ob.removeDups(s) << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
