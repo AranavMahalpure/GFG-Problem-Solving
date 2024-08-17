c4b4098950782077f666b98aaf5a0be6818a8dd3
Arnav Mahalpure <100768252+AranavMahalpure@users.noreply.github.com>
2024-08-17 16:24:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth Smallest/kth-smallest.cpp
@@ -0,0 +1,65 @@
+//{ Driver Code Starts
+// Initial function template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    // arr : given array
+    // k : find kth smallest element and return using this function
+    int kthSmallest(vector<int> &arr, int k) {
+        // code here
+     priority_queue<int>pq;
+    // Add elements to the heap
+    for(int i = 0; i < arr.size(); i++) {
+        // Push the element into the max-heap
+        pq.push(arr[i]);
+
+        // If the heap size exceeds k, pop the largest element (top of the heap)
+        if(pq.size() > k) {
+            pq.pop();
+        }
+    }
+
+    // The top of the heap will be the kth smallest element
+    return pq.top();
+}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int test_case;
+    cin >> test_case;
+    cin.ignore();
+    while (test_case--) {
+
+        int k;
+        vector<int> arr, brr, crr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            crr.push_back(number);
+        }
+        k = crr[0];
+        int n = arr.size();
+        Solution ob;
+        cout << ob.kthSmallest(arr, k) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
