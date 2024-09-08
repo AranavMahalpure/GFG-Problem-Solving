144c8b0184f6d089f82dab014276b8247b4487cf
Arnav Mahalpure <100768252+AranavMahalpure@users.noreply.github.com>
2024-09-08 17:59:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
@@ -1,61 +1,66 @@
 //{ Driver Code Starts
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 
 // } Driver Code Ends
 // Function to return minimum number of jumps to end of array
 
-class Solution{
+class Solution {
   public:
-    int minJumps(int arr[], int n){
+    int minJumps(vector<int>& arr) {
         // Your code here
-        if(n == 1){
-            return 0;
-        }
-        if(arr[0] == 0){
-            return -1;
-        }
-        int rng = arr[0];
-        int st = arr[0];
-        int jump = 1;
-        for(int i = 1; i < n; i++){
-            if(i == n - 1){
-                return jump;
-            }
-            rng = max(rng, i + arr[i]);
-            st--;
-            if(st == 0){
-                jump++;
-                
-                if(rng <= i){
-                    return -1;
+        int jumps = 0;
+        int n = arr.size();
+        
+        for(int i=0;i<n;i++){
+            //i-->maxiInd
+            int maxiInd = -1;
+            int maxi = INT_MIN;
+            for(int j=i+1;j<i+1+arr[i];j++){
+                if(j >= n-1){
+                    //1 jump for i-->n-1
+                    jumps++;
+                    return jumps;
                 }
-                
-                st = rng - i;
+                if(maxi <= arr[j]+j){
+                    maxi = arr[j]+j;
+                    maxiInd = j;
+                }
+            }
+            // 1 jump for i--> maxiInd
+            jumps++;
+            i = maxiInd-1;
+            if(maxiInd == -1){
+                //if you are at ind which is zero valued
+                return -1;
             }
         }
         
-        return -1;
+        return jumps;
     }
 };
 
 
 //{ Driver Code Starts.
 
-int main()
-{
+int main() {
     int t;
-    cin>>t;
-    while(t--)
-    {
-        int n,i,j;
-        cin>>n;
-        int arr[n];
-        for(int i=0; i<n; i++)
-            cin>>arr[i];
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        int n, i, j;
+        vector<int> arr;
+        string ip;
+        int number;
+        getline(cin, ip);
+        stringstream ss(ip);
+
+        while (ss >> number) {
+            arr.push_back(number);
+        }
         Solution obj;
-        cout<<obj.minJumps(arr, n)<<endl;
+        cout << obj.minJumps(arr) << endl;
     }
     return 0;
 }
